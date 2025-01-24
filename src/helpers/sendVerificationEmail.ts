37ba3da2bb06dc881eef264f69cfe1962743d3b0
Darshan Subedi <subedidarshan02@gmail.com>
2025-01-24 20:14:00 +0530

    Implement user signup with verification email functionality

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,24 @@
+import { resend } from "@/lib/resend";
+import ApiResponse from "@/types/ApiResponse";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+export async function sendVerificationEmail(email:string, username:string, otp:string):Promise<ApiResponse> {
+    try {
+      const { data, error } = await resend.emails.send({
+        from: process.env.SENDER_EMAIL!,
+        to: email,
+        subject: 'True Feedback Verification Code',
+        react: VerificationEmail({username, otp}),
+      });
+  
+      if (error) {
+        throw error
+      }
+  
+      return {success: true, message: "Verification email sent"}
+    } catch (error) {
+        console.log("Error sending verification email", error);
+        
+      return {success: false, message: "Error sending verification email :: "+error};
+    }
+  }
---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,8 @@
+import { Message } from "@/models/user.model";
+
+export default interface ApiResponse {
+    success: boolean,
+    message: string,
+    isAcceptingMessages?: boolean,
+    messages?: Array<Message>
+}
