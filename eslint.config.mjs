71aa1b508daccf6cab191d0832e4eeae46775f68
Darshan Subedi <subedidarshan02@gmail.com>
2025-01-31 21:53:58 +0530

    Update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals"],
+    extends: ["next"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
   }),
 ];
 
